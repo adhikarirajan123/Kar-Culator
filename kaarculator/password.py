cbf00c84dedaff663e2a507c6432e02dd4bb4498
rajan <razanmandal2018@gmail.com>
2022-04-29 12:20:09 +0545

    changed

--- kaarculator/password.py
+++
@@ -1,4 +0,0 @@
-password="Rajan@12345#"
-email=""
-email_password=""
-app_password=""
--- news/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import NewsModel, Category
-
-admin.site.register(NewsModel)
-admin.site.register(Category)
